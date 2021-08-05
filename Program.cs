7e28eb77a8ae7c1ea2ba7941dcaca2562f8ce7e3
SKILLGHOST0 <86680129+SKILLGHOST0@users.noreply.github.com>
2021-08-05 20:20:03 +0300

    homework 6
    homework 6

--- Program.cs
+++ Program.cs
@@ -1,72 +1,29 @@
-﻿//урок 2 
-using System;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
-class program{
-        static void Main(){
-        //задание 1 
-      double firstvalue, secondvalue;
-
-      Console.WriteLine("минимальная температура");
-
-      firstvalue = double.Parse(Console.ReadLine());
-
-      Console.WriteLine("максимальная температура");
-
-      secondvalue = double.Parse(Console.ReadLine());
-
-      double result = (firstvalue + secondvalue) / 2;
-
-      Console.WriteLine("средняя температура " + result);
-      // задание 2 
-      Console.WriteLine("введите порядковый номер месяца");
-
-      int month;
-
-      month = int.Parse(Console.ReadLine());
-      
-      if(month == 1)
-      Console.WriteLine("январь");
-      if(month == 2)
-      Console.WriteLine("февраль");
-      if(month == 3)
-      Console.WriteLine("март");
-      if(month == 4)
-      Console.WriteLine("апрель");
-      if(month == 5)
-      Console.WriteLine("май");
-      if(month == 6)
-      Console.WriteLine("июнь");
-      if(month == 7)
-      Console.WriteLine("июль");
-      if(month == 8)
-      Console.WriteLine("август");
-      if(month == 9)
-      Console.WriteLine("сентябрь");
-      if(month == 10)
-      Console.WriteLine("октябрь");
-      if(month == 11)
-      Console.WriteLine("ноябрь");
-      if(month == 12)
-      Console.WriteLine("декабрь");
-       
-    
-      // задание 3
-      Console.WriteLine("проверка числа на четность");
-       
-      int a;
-
-      a = int.Parse(Console.ReadLine());
-
-      if(a % 2 == 0)
-      Console.WriteLine("число четное ");
-      else
-      {
-          Console.WriteLine("число не четное");
-      }
-
-      // задание 4
-      
-
+namespace home_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var procList = Process.GetProcesses();
+            foreach (var proc in procList)
+            {
+                Console.WriteLine($"{proc.Id} {proc.ProcessName} {proc.BasePriority}");
+            }
+            Console.Write("name = ");
+            var name = Console.ReadLine();
+            try
+            {
+                procList.First(p => p.ProcessName.ToLower() == name.ToLower()).Kill();
+                Console.WriteLine($"{name} deleted!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Process {name} not found!");
+            }
+        }
     }
 }
-
